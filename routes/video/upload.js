8a654f35a7effa23485c3ad240ebdf760c0d27fb
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-03-28 18:45:46 +0200

    b4 removing hidden csurf input

--- routes/video/upload.js
+++ routes/video/upload.js
@@ -7,14 +7,19 @@
  * @requires express
  * @requires Video
  * @requires csurf
+ * @requires formidable
  */
 
 'use strict'
 
 const router = require('express').Router()
 // const Video = require('../../models/Video')
+
 const csrf = require('csurf')
 const csrfProtection = csrf()
+
+const formidable = require('formidable')
+const form = formidable.IncomingForm()
 
 router.route('/upload')
     // renders upload form, only for logged in users
@@ -25,6 +30,30 @@ router.route('/upload')
         } else {
             res.render('video/upload', {
                 csrfToken: req.csrfToken()
+            })
+        }
+    })
+
+    // saves video upload in DB
+    // Inspired by pages 95-96 in the book "Web Development with Node & Express"
+    // by Ethan Brown
+    .post(csrfProtection, async (req, res) => {
+        if (!req.session.username) {
+            res.status(403)
+            res.render('error/403')
+        } else {
+            
+            form.parse(req, (err, fields, file) => {
+                if (err) {
+                    req.session.flash = {
+                        type: 'error',
+                        text: 'Something went wrong'
+                    }
+                    res.redirect('/upload') 
+                }
+
+            console.log(req.fields)
+            console.log(req.files)
             })
         }
     })
