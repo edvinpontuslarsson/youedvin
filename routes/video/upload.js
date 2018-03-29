4854195e01d949a12a0af1244fc9358d7a7b54e7
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-03-29 20:34:47 +0200

    Saves video file to DB, but filename incorrect

--- routes/video/upload.js
+++ routes/video/upload.js
@@ -23,6 +23,9 @@ const router = require('express').Router()
 const Lib = require('../../lib/Lib')
 const lib = new Lib()
 
+const csrf = require('csurf')
+const csrfProtection = csrf()
+
 // const Video = require('../../models/Video')
 
 const multer = require('multer')
@@ -33,13 +36,36 @@ const GridFsStorage = require('multer-gridfs-storage')
 const Grid = require('gridfs-stream')
 const connection = mongoose.connection
 
-const csrf = require('csurf')
-const csrfProtection = csrf()
+// using this similarly to their documentation:
+// https://www.npmjs.com/package/multer-gridfs-storage
+connection.once('open', () => {
+    const gfs = Grid(connection.db, mongoose.mongo)
+    gfs.collection('videoData')
+    console.log('Ready for video uploads')
+})
 
-/*
-const formidable = require('formidable')
-const form = formidable.IncomingForm()
-*/
+const nrString = lib.randomNrs()
+
+const storage = new GridFsStorage({
+    url: process.env.dbURL,
+    file: (req, file) => {
+        return new Promise((resolve, reject) => {
+            console.log(nrString + path.extname(file.originalname))
+            // for security reasons, I change the name of the file
+            // const fileName = nrString + path.extname(file.originalname)
+
+            const fileInfo = {
+                fileName: nrString + path.extname(file.originalname),
+                bucketName: 'videoData'
+            }
+            resolve(fileInfo)
+        })
+    }
+})
+
+const upload = multer({
+    storage
+})
 
 router.route('/upload')
     // renders upload form, only for logged in users
@@ -55,30 +81,12 @@ router.route('/upload')
     })
 
     // saves video to DB, only for logged in users
-    .post(csrfProtection, (req, res) => {
+    .post(csrfProtection, upload.single('video'), (req, res) => {
         if (!req.session.username) {
             res.status(403)
             res.render('error/403')
         } else {
-            // inspired by this demo: https://www.youtube.com/watch?v=3f5Q9wDePzY
-            const gfs = Grid(connection.db, mongoose.mongo)
-            gfs.collection('video')
-
-            // using this similarly to their documentation:
-            // https://www.npmjs.com/package/multer-gridfs-storage
-            const storage = new GridFsStorage({
-                url: process.env.dbURL,
-                file: (req, file) => {
-                    return new Promise((res, rej) => {
-
-                        // for security reasons, I change the name of the file
-                        const fileName = lib.randomNrs() + 
-                        path.extname(file.originalname)
-
-                        
-                    })
-                }
-            })
+            console.log(req.file)            
 
             req.session.flash = {
                 type: 'success',
