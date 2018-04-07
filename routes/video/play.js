f35fc6821086a0e1997210660cd83f4fc1a6db3f
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-04-07 20:29:19 +0200

    Gets video and you can play the video

--- routes/video/play.js
+++ routes/video/play.js
@@ -16,12 +16,31 @@ const multer = require('multer')
 const GridFsStorage = require('multer-gridfs-storage')
 let gfs
 
-// Inspired by: https://www.youtube.com/watch?v=3f5Q9wDePzY
+/**
+ * Connects gridfs-stream to the database
+ * Inspired by: https://www.youtube.com/watch?v=3f5Q9wDePzY&t=2422s
+ */
 connection.once('open', () => {
   gfs = Grid(connection.db, mongoose.mongo)
   gfs.collection('uploads')
   console.log('Ready for fetching video')
 })
+
+/**
+ * Creates a readstream to the video file
+ * Inspired by: https://www.youtube.com/watch?v=3f5Q9wDePzY&t=2422s
+ */
+router.route('/video/:id')
+  .get((req, res) => {
+    const fileName = req.params.id
+
+    gfs.files.findOne({
+      filename: fileName
+    }, (error, videoFile) => {
+      const readStream = gfs.createReadStream(videoFile.filename)
+      readStream.pipe(res)
+    })
+  })
 
 router.route('/play/:id')
   .get(async (req, res) => {      
@@ -30,29 +49,13 @@ router.route('/play/:id')
       const videoInfo = await lib.getVideoInfo(fileName)
       const title = videoInfo.title
 
-      // test without await, should restructure
-      // watch until end, see if I need stream
-
-      const videoFile = await fetchVideo(fileName)
-
-      console.log(videoFile)
-
-      console.log('Hello')
-
-      res.render('video/play', { 
-        title, videoFile
+      gfs.files.findOne({
+        filename: fileName
+      }, (error, videoFile) => {
+        res.render('video/play', { 
+          title, videoFile
+        })
       })
   })
-
-/**
- * Fetches the video file
- */
-function fetchVideo (fileName) {
-  // should have if no file, return 404
-  return gfs.files.findOne({
-    filename: fileName
-  })
-}
-
 
 module.exports = router
