3105a48d0b9e23d56d506cd8ecd69999f328c228
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-04-19 16:03:37 +0200

    Sorts after newest vids

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ app.use((req, res, next) => {
   delete req.session.flash
 
   // updates header menu for logged in users
-  res.locals.loggedIn = req.session.username
+  res.locals.username = req.session.username
 
   next()
 })
