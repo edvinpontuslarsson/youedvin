2ebb53724c6cf466e914777bf42c851ea72b7770
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-04-19 14:42:35 +0200

    b4 trying grid

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -9,12 +9,12 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-/**
- * For connecting to a database
- * Inspired by demo code in the course 1dv023
- * https://github.com/1dv023/syllabus/tree/master/lectures/03/demo
- */
 module.exports = {
+  /**
+   * For connecting to a database
+   * Inspired by demo code in the course 1dv023
+   * https://github.com/1dv023/syllabus/tree/master/lectures/03/demo
+   */
   dbConnect: () => {
     const db = mongoose.connection
 
