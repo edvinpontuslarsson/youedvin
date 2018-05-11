e39aa545d2f9db31dbb1e407f37e63fb7eeb5cc4
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-05-11 18:41:05 +0200

    Sets limit for username length

--- routes/user/userEdit.js
+++ routes/user/userEdit.js
@@ -17,9 +17,21 @@ router.route('/useredit/:id')
         // server error
         if (error) { throw error }
 
-            
-        }
-        )
+        if (req.session.userid !== user._id)
+        {
+            res.status(403)
+            res.render('error/403')
+
+        // if everything is OK
+        } else {
+            res.status(200)
+            res.header({ csrfToken: req.csrfToken() })
+            res.render('user/userEdit', {
+                username: user.username,
+                id: user._id
+            })
+            }
+        })
     })
 
 module.exports = router
