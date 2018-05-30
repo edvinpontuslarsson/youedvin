233ff25c1d3c010fab89ab2873c0bbca7828098c
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-05-30 21:59:58 +0200

    Cleans code surprise.js

--- routes/video/surprise.js
+++ routes/video/surprise.js
@@ -1,5 +1,5 @@
 /**
- * Redirects to random video
+ * Surprise route
  */
 
 'use strict'
@@ -8,23 +8,27 @@ const router = require('express').Router()
 const VideoAmount = require('../../models/VideoAmount')
 const Lib = require('../../lib/Lib')
 
-router.route('/surprise')
-  .get(async (req, res) => {
-    const videoAmount = await VideoAmount.findOne({
-      name: 'VideoAmount'
-    })
-
-    if (!videoAmount) { return res.render('error/404') }
-
-    const amount = videoAmount.amount
-    const skip = Lib.make.randomInteger(0, amount)
-
-    // selects a random video by skipping a random amount
-    const randomVideo = await Lib.get.newestVideos(1, skip)
-
-    if (!randomVideo[0]) { return res.render('error/404') }
-
-    res.redirect(`/play/${randomVideo[0].fileName}`)
+// Redirects to random video
+router.route('/surprise').get(async (req, res) => {
+  const videoAmount = await VideoAmount.findOne({
+    name: 'VideoAmount'
   })
+
+  if (!videoAmount) {
+    return res.render('error/404')
+  }
+
+  const amount = videoAmount.amount
+  const skip = Lib.make.randomInteger(0, amount)
+
+  // selects a random video by skipping a random amount
+  const randomVideo = await Lib.get.newestVideos(1, skip)
+
+  if (!randomVideo[0]) {
+    return res.render('error/404')
+  }
+
+  res.redirect(`/play/${randomVideo[0].fileName}`)
+})
 
 module.exports = router
