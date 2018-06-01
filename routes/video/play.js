23afc15424eee0c8aa8ea971cdb396d9d8413c2e
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-06-01 15:05:40 +0200

    fs vid gets played

--- routes/video/play.js
+++ routes/video/play.js
@@ -5,43 +5,7 @@
 'use strict'
 
 const router = require('express').Router()
-const mongoose = require('mongoose')
 const Lib = require('../../lib/Lib')
-
-const connection = mongoose.connection
-const Grid = require('gridfs-stream')
-let gfs
-
-/**
- * Connects gridfs-stream to the database
- * Inspired by: https://www.youtube.com/watch?v=3f5Q9wDePzY&t=2422s
- */
-connection.once('open', () => {
-  gfs = Grid(connection.db, mongoose.mongo)
-  gfs.collection('uploads')
-  console.log('Ready for fetching videos')
-})
-
-/**
- * Creates a readstream to the video file
- * Inspired by: https://www.youtube.com/watch?v=3f5Q9wDePzY&t=2422s
- *//*
-router.route('/video/:id')
-  .get((req, res) => {
-    const fileName = req.params.id
-
-    gfs.files.findOne({
-      filename: fileName
-    }, (error, videoFile) => {
-      if (error) {
-        res.status(500)
-        res.redirect('/')
-      }
-
-      const readStream = gfs.createReadStream(videoFile.filename)
-      readStream.pipe(res)
-    })
-  })*/
 
 router.route('/play/:id')
   
